e4e936b8c8b6dc734e7e2cacbbe487d0a5ce7e96
Timothy Ng <timothyng2008@me.com>
2018-10-29 20:35:07 +0800

    - feat: Add static arrays for greetings and press button label text

--- Hello-World-World-Edition/ViewController.swift
+++ Hello-World-World-Edition/ViewController.swift
@@ -13,6 +13,40 @@ class ViewController: UIViewController {
 //----------------------------------------------------------------------------------
 // MARK:- Properties
     
+    var greetings = ["Welcome",
+                     "Velkommen",
+                     "Welkom",
+                     "歡迎",
+                     "Bienvenue",
+                     "Willkommen",
+                     "ようこそ",
+                     "Benvenuto",
+                     "Välkommen",
+                     "환영하다",
+                     "Bienvenido",
+                     "欢迎",
+                     "Tervetuloa",
+                     "Bem-vindo",
+                     "Добро пожаловать"
+                  ]
+    
+    var pressMes = ["Press Me",
+                    "Trykk på Me",
+                    "Druk op mij",
+                    "按我",
+                    "Appuyez-moi",
+                    "Drück mich",
+                    "私を押す",
+                    "Premi Me",
+                    "Tryck på Me",
+                    "나를 압박해라",
+                    "Presionarme",
+                    "按我",
+                    "Paina Me",
+                    "Pressione-me",
+                    "Нажмите Me"
+                  ]
+    
 //----------------------------------------------------------------------------------
 // MARK:- Outlets
     @IBOutlet weak var welcomeLabel: UILabel!
